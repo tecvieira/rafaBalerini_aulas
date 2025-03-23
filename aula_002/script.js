435bfdfd7587d5fc227e5ed35d5203f1aab9e9a7
Robson Vieira <tecvander.vieira@gmail.com>
2025-03-22 22:23:01 -0300

    2ª tentando fazer css

--- aula_002/script.js
+++ aula_002/script.js
@@ -1,12 +1,19 @@
 function adicionarTarefa() {
-   //let mensagem = "Tarefa adicionada com sucesso!";
-    let inpuTarefa = inputTarefa.value
-    document.getElementById("mensagem").textContent = mensagem;
 
-    let ListaTarefas = document.getElementById("listaTarefas")
-    let novaTarefa = document.createElement("li")
+  //recebe valor do input do usuário
+  let inputTarefa = document.getElementById("inputTarefa")
+  let tarefa = inputTarefa.value
 
-    ListaTarefas.appendChild(novaTarefa)
+  //cria novo item (li) e insere na (lista ul)
+  let listaTarefas = document.getElementById("listaTarefas")
+  let novaTarefa = document.createElement("li")
+  novaTarefa.textContent = tarefa
+  listaTarefas.appendChild(novaTarefa)
 
-    inputTarefa.vale
-  }
+  //mensagem de tarefa adicionada com sucesso
+  let mensagem = "Tarefa adicionada com sucesso!"
+  document.getElementById("mensagem").textContent = mensagem
+
+  //limpa o input do usuário
+  inputTarefa.value = ""
+}
