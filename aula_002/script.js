022b48541b7753fd8a5c3e275b20f6a943f64211
Robson Vieira <tecvander.vieira@gmail.com>
2025-03-22 21:36:27 -0300

    criando java script 1

---
+++ aula_002/script.js
@@ -0,0 +1,12 @@
+function adicionarTarefa() {
+   //let mensagem = "Tarefa adicionada com sucesso!";
+    let inpuTarefa = inputTarefa.value
+    document.getElementById("mensagem").textContent = mensagem;
+
+    let ListaTarefas = document.getElementById("listaTarefas")
+    let novaTarefa = document.createElement("li")
+
+    ListaTarefas.appendChild(novaTarefa)
+
+    inputTarefa.vale
+  }
