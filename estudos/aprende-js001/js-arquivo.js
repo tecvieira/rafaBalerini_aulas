1c420a11d17dba721e9671c3974f10fc60b1cdc2
Robson Vieira <tecvander.vieira@gmail.com>
2025-03-31 05:28:47 -0300

    javascript funcionou1

--- estudos/aprende-js001/js-arquivo.js
+++ estudos/aprende-js001/js-arquivo.js
@@ -1,17 +1,29 @@
 function crieCartao(){
+    //mensagem após clicar no botão
     let mensagem = "Dados enviado com sucesso"
+    //busca no html o dado inserido em "empresa"
     let imputEmpresa = document.getElementById
     ("empresa")
+    //busca no html o dado inserido em "nome"
     let imputNome = document.getElementById("nome")
+    //busca no html o dado inserido em "telefone"
     let imputNumero = document.getElementById("telefone")
-
+    
     let minhaEmpresa = imputEmpresa.value
     let minhaNome = imputNome.value
     let minhaNumero = imputNumero.value
-    document.getElementById(mensagem).textContent = mensagem;
 
-
+    //aqui escreve a "mensagem" no id=mensagemP
+    document.getElementById("mensagemP").textContent = mensagem
+    document.getElementById("cardEmpresa").textContent = minhaEmpresa
+    document.getElementById("cardNome").textContent = minhaNome
+    document.getElementById("cardNumero").textContent = minhaNumero
     
 
+    empresa.value = ""
+    nome.value = ""
+    telefone.value = ""
+    
 
+    
 }
