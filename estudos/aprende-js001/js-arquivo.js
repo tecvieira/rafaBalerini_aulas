4861960c4ba32c9bf9c40156fea756d2af610960
Robson Vieira <tecvander.vieira@gmail.com>
2025-03-30 22:57:27 -0300

    ajustando js 1

--- estudos/aprende-js001/js-arquivo.js
+++ estudos/aprende-js001/js-arquivo.js
@@ -1,6 +1,7 @@
 function crieCartao(){
     let mensagem = "Dados enviado com sucesso"
-    let imputEmpresa = document.getElementById("empresa")
+    let imputEmpresa = document.getElementById
+    ("empresa")
     let imputNome = document.getElementById("nome")
     let imputNumero = document.getElementById("telefone")
 
@@ -8,6 +9,7 @@ function crieCartao(){
     let minhaNome = imputNome.value
     let minhaNumero = imputNumero.value
     document.getElementById(mensagem).textContent = mensagem;
+
 
     
 
