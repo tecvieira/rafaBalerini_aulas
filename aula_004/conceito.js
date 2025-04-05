0cfa09c92583c5b5f5317affaff80ae3a777ae56
Robson Vieira <tecvander.vieira@gmail.com>
2025-04-05 18:23:58 -0300

    aula 4 criando um for e array

---
+++ aula_004/conceito.js
@@ -0,0 +1,10 @@
+// Maneira basica de declarar uma variável
+// fruta = "banana"
+// alert(fruta)
+
+frutas = ["maca", "banana", "mamao"]
+// assim ele vai mostrar todas as frutas da lista
+//alert(frutas)
+// este comando mostra o indice do elemento
+// alert(frutas.indexOf("maca"))
+alert(frutas.length)
